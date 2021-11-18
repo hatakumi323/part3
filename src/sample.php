ca9b8f6d8104b55a35a43f5eee6b2b4fdffbe373
hatakumi323 <65800363+hatakumi323@users.noreply.github.com>
2021-11-18 23:44:56 +0900

    for fizzbuzz

--- src/sample.php
+++ src/sample.php
@@ -2,17 +2,15 @@
 // 3の倍数のときfizz、5の倍数のときbuzz、15の倍数の時、fizzbuzzと返す、echoFizzBuzzという関数を作成してください。
 // これ以外の時は、NOT FOUND! と返してください。
 
-function echoFizzBuzz($fizzbuzz)
-{
-  if ($fizzbuzz % 3 == 0) {
-    return "Fizz";
-  } else if ($fizzbuzz % 5 == 0) {
-    return "Buzz";
-  } else if ($fizzbuzz % 3 == 0 || $fizzbuzz % 5 == 0) {
-    return "FizzBuzz";
+for ($i = 1; $i <= 100; $i++) {
+  if ($i % 3 == 0 && $i % 5 == 0) {
+    echo "FizzBuzz" . PHP_EOL;
+  } else if ($i % 3 == 0) {
+    echo "Fizz" . PHP_EOL;
+  } else if ($i % 5 == 0) {
+    echo "Buzz" . PHP_EOL;
   } else {
-    return "NOT FOUND";
+    echo $i . PHP_EOL;
   }
+  echo " ";
 }
-
-echo echoFizzBuzz(3) . PHP_EOL;
