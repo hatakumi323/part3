7a650bcc05f725c98320ba26f51f28e112ea6010
hatakumi323 <65800363+hatakumi323@users.noreply.github.com>
2021-11-18 23:38:41 +0900

    fizzbuzz

--- src/sample.php
+++ src/sample.php
@@ -1,20 +1,18 @@
 <?php
-// ローンの金額initialLoan、期日までに払われたかどうかをブーリアンで表したdidPayOnTime を入力として受け取り、$2.5 の手数料とローンの利子を請求する、interestsPaid という関数を作成します。期限内にお金が支払われなかった場合、15% の利子となり、支払われた場合は 2% の利子が請求されます。
+// 3の倍数のときfizz、5の倍数のときbuzz、15の倍数の時、fizzbuzzと返す、echoFizzBuzzという関数を作成してください。
+// これ以外の時は、NOT FOUND! と返してください。
 
-function interestsPaid($initialLoan, $didPayOnTime)
+function echoFizzBuzz($fizzbuzz)
 {
-  $percentLate = 1.15;
-  $percentDefault = 1.02;
-  $serviceFee = 2.5;
-  $total = $initialLoan;
-
-  if ($didPayOnTime) {
-    $total = $total * $percentDefault;
+  if ($fizzbuzz % 3 == 0) {
+    return "Fizz";
+  } else if ($fizzbuzz % 5 == 0) {
+    return "Buzz";
+  } else if ($fizzbuzz % 3 == 0 || $fizzbuzz % 5 == 0) {
+    return "FizzBuzz";
   } else {
-    $total = $total * $percentLate;
+    return "NOT FOUND";
   }
-  return $total + $serviceFee;
 }
 
-echo interestsPaid(100, true) . PHP_EOL;
-echo interestsPaid(100, false) . PHP_EOL;
+echo echoFizzBuzz(3) . PHP_EOL;
