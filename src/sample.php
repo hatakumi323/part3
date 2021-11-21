39685db89e4359008b23cf9b1dd0cda29b3c15a9
hatakumi323 <65800363+hatakumi323@users.noreply.github.com>
2021-11-21 11:26:57 +0900

    karaoke Fee

--- src/sample.php
+++ src/sample.php
@@ -1,38 +1,44 @@
 <?php
-// さまざまな惑星で物体を yマイルに渡って移動するために必要なエネルギー量を計算してみましょう。
+// 複雑な処理が必要な場合は、やるべき処理を分解してそれぞれ関数を作ります。
+// カラオケの料金を計算する関数、学校があるか判定する関数、閏年か判定する関数の3つをつくり、最終的なカラオケ料金を計算する関数getKaraokeFee()の中から呼び出します。
 
-function forceNewtons($kg, $mpss)
+function getKaraokeFee($day, $isHoliday, $lastDigits, $num)
 {
-  return $kg * $mpss;
+  $price = calculateFee($num);
+  if (!isThereSchool($day, $isHoliday)) $price *= 4;
+  if (isLeapYear($lastDigits)) $price /= 2;
+
+  return $price;
 }
 
-function planetGravityMpss($planet)
+//　カラオケの料金を計算する関数
+function calculateFee($people)
 {
-  if ($planet == "Earth") {
-    return 9.80665;
-  }
-  if ($planet == "Jupiter") {
-    return 24.79;
-  }
-  if ($planet == "Neptune") {
-    return 11.15;
-  }
-  return 0;
+  $perPerson;
+
+  if ($people <= 3) $perPerson = 8;
+  else if ($people < 10) $perPerson = 6;
+  else $perPerson = 5;
+
+  return $perPerson * $people;
 }
 
-function poundsTokg($pounds)
+// 学校があるか判定する関数
+function isThereSchool($day, $isHoliday)
 {
-  return $pounds * 0.453592;
+  if ($day == "Sunday" || $day == "Saturday" || $isHoliday) return false;
+  else return true;
 }
 
-function joulesByWork($newtons, $meters)
+// 閏年かどうか判定する関数
+function isLeapYear($year)
 {
-  return $newtons * $meters;
+  if ($year % 400 == 0) return true;
+  else if ($year % 100 == 0) return false;
+  else if ($year % 4 == 0) return true;
+  return false;
 }
 
-function energyMovingPoundsByPlanet($planet, $pounds, $meters)
-{
-  return joulesByWork(forceNewtons(poundsToKg($pounds), planetGravityMpss($planet)), $meters);
-}
-// 木星で65ポンドの物体を35メートル移動するのに必要なエネルギー量を返します。
-echo energyMovingPoundsByPlanet("Jupiter", 65, 35) . PHP_EOL;
+echo getKaraokeFee("Sunday", true, 1900, 5) . PHP_EOL;
+
+echo (getKaraokeFee("Saturday", false, 1900, 2)) . PHP_EOL;
