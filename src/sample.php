5b782d5c5418a9258ff579d31f03a401f91a7142
hatakumi323 <65800363+hatakumi323@users.noreply.github.com>
2021-11-23 15:00:46 +0900

    recursion

--- src/sample.php
+++ src/sample.php
@@ -1,16 +1,44 @@
 <?php
 // 文字列が与えられるので、再帰を用いて、文字列の長さをカウントする、lengthStringという関数を作成してください。
 
-function countSquare($x, $y)
+// function lengthString($string)
+// {
+//   if ($string == "") {
+//     return 0;
+//   }
+//   return 1 + lengthString(substr($string, 1));
+// }
+
+// echo lengthString("Hello") . PHP_EOL;
+
+// 同じサイズの文字列s1、s2が与えられるので、それぞれの文字を1->2の順番で交互に組み合わせる、mergeStringという関数
+
+// function mergeString($s1, $s2)
+// {
+//   if ($s1 == "" || $s2 == "") {
+//     return "";
+//   }
+
+//   return $s1[0] . $s2[0] . mergeString(substr($s1, 1), (substr($s2, 1))) . PHP_EOL;
+// }
+
+// 別解
+// 入力がstr型、出力がint型なので、パラメーターを追加してより多くのデータを追跡することもできます。
+
+function lenString($string)
 {
-  // ここから書きましょう
-  if ($x == $y) {
-    return $x;
-  } else if ($x > $y) {
-    return countSquare($x - $y, $y);
-  } else {
-    return countSquare($x, $x - $y);
-  }
+  // パラメータを追加する
+  return lenStringHelper($string, 0);
 }
 
-echo countSquare(28, 32);
+function lenStringHelper($string, $count)
+{
+  if ($string == "") {
+    return $count;
+  }
+
+  return lenStringHelper(substr($string, 1), $count + 1);
+}
+
+echo lenString("hello") . PHP_EOL;
+echo lenString("helloworld") . PHP_EOL;
